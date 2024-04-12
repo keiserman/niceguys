500a3bc3e3b3e060b01813b86922dadd3b1f6378
Kevin <eisermanka@gmail.com>
2024-04-12 14:12:52 -0400

    Add aspect ratio to video element

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  playPauseVideo();
+  initVideos();
 });
 
 function initAnimations() {
   gsap.from(".video-wrapper", { scale: 0, duration: 0.3 });
 }
 
-function playPauseVideo() {
-  let videos = document.querySelectorAll("video");
+function initVideos() {
+  const videos = document.querySelectorAll("video");
+
   videos.forEach((video) => {
-    // We can only control playback without insteraction if video is mute
     video.muted = true;
-    // Play is a promise so we need to check we have it
     let playPromise = video.play();
     if (playPromise !== undefined) {
       playPromise.then((_) => {
@@ -30,28 +29,5 @@ function playPauseVideo() {
         observer.observe(video);
       });
     }
-  });
-}
-
-function initVideos() {
-  const videos = document.querySelectorAll("video");
-  videos.forEach((video) => {
-    video.muted = true;
-    video.loop = true;
-
-    const observer = new IntersectionObserver(
-      (entries, observer) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            video.play();
-          } else {
-            video.pause();
-          }
-        });
-      },
-      { threshold: 0.5 }
-    );
-
-    observer.observe(video);
   });
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: ["@tailwindcss/aspect-ratio"],
+  plugins: [],
 };
